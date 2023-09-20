2e6ad85269e32e1d03df8b3068fb6b7613b0bc16
Tjelle de Bree <tjelledebree@gmail.com>
2023-09-20 16:02:47 +0200

    gecooked

--- MandelbrotProgramm-TF/MandelbrotProgramm-TF/Program.cs
+++ MandelbrotProgramm-TF/MandelbrotProgramm-TF/Program.cs
@@ -14,14 +14,16 @@ namespace MandelbrotProgramm_TF
             scherm.Controls.Add(mandlebrotOutput);
             mandlebrotOutput.Size = new Size(plaatje.Width, plaatje.Height);
             mandlebrotOutput.Location = new Point(700, 100);
+            mandlebrotOutput.Image = plaatje;
             mandlebrotOutput.BackColor = Color.LightGray;
 
+            
 
 
 
-            double a = 0;
-            double b = 0;
 
+            
+/*
             double Pythagoras()
             { 
                 return Math.Sqrt(a * a + b * b);
@@ -33,15 +35,34 @@ namespace MandelbrotProgramm_TF
                 a = tijdelijk;
                 b = 2 * a * b;
             }
-
+*/
 
 
             for (int x = 0; x < mandlebrotOutput.Width; x++)
             {
                 for (int y = 0; y < mandlebrotOutput.Height; y++)
                 {
-                    plaatje.SetPixel(x,y, Color.Black);
-                    //double a = ((double)(x - mandlebrotOutput.Width / 2) / (mandlebrotOutput.Width / 4));
+                    
+                    double a = ((double)(x - mandlebrotOutput.Width / 2) / (mandlebrotOutput.Width / 4));
+                    double b = ((double)(y - mandlebrotOutput.Height / 2) / (mandlebrotOutput.Height /4));
+
+                    double z = 0;
+                    double c = 0;
+
+
+                    
+                    for (int i = 1; i <= 100; i++)
+                    {
+                        a = (double)((a * a) - (b * b)) + x;
+                        b = (double)(2 * a * b + y);
+                        z = z + a + b;
+                        if (Math.Sqrt(a * a + b * b) > 2) 
+                        {
+                            plaatje.SetPixel(x, y, Color.Black);                      
+                            break;
+                        }
+
+                    }
                 }
             }
 
