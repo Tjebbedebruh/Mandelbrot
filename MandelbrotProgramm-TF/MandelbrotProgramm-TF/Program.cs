b15729a1c4d3f9ed18146d6d336e2e0f3522ac9f
FlorisMohammad <145458838+FlorisMohammad@users.noreply.github.com>
2023-09-20 14:05:03 +0200

    Begin gemaakt

--- MandelbrotProgramm-TF/MandelbrotProgramm-TF/Program.cs
+++ MandelbrotProgramm-TF/MandelbrotProgramm-TF/Program.cs
@@ -1,3 +1,9 @@
 using System;
 using System.Windows.Forms;
 using System.Drawing;
+using System.DirectoryServices.ActiveDirectory;
+
+Form scherm = new Form();
+scherm.ClientSize = new Size(1000, 1000);
+scherm.Text = "MandelbrotprogrammaTF";
+
